0dc952cc9a0072dbfa296b3dcf08c3f9aeba85e0
la8457478 <364249915@qq.com>
2018-12-04 15:11:15 +0800

    67

---
+++ renren-fast-vue/src/views/modules/course/video.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <div class="player">
+      <video-player  class="video-player vjs-custom-skin"
+                     ref="videoPlayer"
+                     :playsinline="true"
+                     :options="playerOptions"
+                     @play="onPlayerPlay($event)"
+                     @pause="onPlayerPause($event)"
+      >
+      </video-player>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        playerOptions: {
+          playbackRates: [0.7, 1.0, 1.5, 2.0], // 播放速度
+          autoplay: false,
+          muted: false,
+          loop: false,
+          preload: 'auto',
+          language: 'zh-CN',
+          aspectRatio: '16:9',
+          fluid: true,
+          sources: [{
+            type: 'video/mp4',
+            src: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4'
+          }],
+          poster: '../../static/images/test.jpg',
+          notSupportedMessage: '此视频暂无法播放，请稍后再试',
+          controlBar: {
+            timeDivider: true,
+            durationDisplay: true,
+            remainingTimeDisplay: false,
+            fullscreenToggle: true
+          }
+        }
+      }
+    }
+  }
+</script>
